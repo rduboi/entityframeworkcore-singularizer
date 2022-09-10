624ddc012ac0a2102af8ce80c26435df2c97021f
Robin DUBOIS <robin.dubois@equans.com>
2022-09-10 08:27:35 +0200

    Add Singularizer.cs and Singularize extension method based on Humanizer

---
+++ EntityFrameworkCore.Singularizer/Singularizer.cs
@@ -0,0 +1,16 @@
+﻿using Humanizer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EntityFrameworkCore.Singularizer;
+
+public static class Singularizer
+{
+    public static void Singularize(this ModelBuilder modelBuilder)
+    {
+        foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            entityType.SetTableName(entityType.Name.Singularize());
+        }
+    }
+}
